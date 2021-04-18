cffb2069d6562426095a9f948c30a527b81de953
Lil Devil <themax.06y@gmail.com>
2021-04-18 14:57:16 +0300

    Подключил библиотеку FancyCandles к тестовому проекту WPF (+ демонстрационный пример)

--- LuckyCurrencyTest/MainWindow.xaml.cs
+++ LuckyCurrencyTest/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows;
+using System.Collections.ObjectModel;
+using FancyCandles;
+using System;
 
 namespace LuckyCurrencyTest
 {
@@ -7,6 +10,18 @@ namespace LuckyCurrencyTest
         public MainWindow()
         {
             InitializeComponent();
+
+            ObservableCollection<ICandle> candles = new ObservableCollection<ICandle>();
+            DateTime t0 = new DateTime(2019, 6, 11, 23, 40, 0);
+            for (int i = 0; i < 100; i++)
+            {
+                double p0 = Math.Round(Math.Sin(0.3 * i) + 0.1 * i, 3);
+                double p1 = Math.Round(Math.Sin(0.3 * i + 1) + 0.1 * i, 3);
+                candles.Add(new Candle(t0.AddMinutes(i * 5),
+                            100 + p0, 101 + p0, 99 + p0, 100 + p1, i));
+            }
+
+            DataContext = candles;
         }
     }
 }
