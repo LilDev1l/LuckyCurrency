5a5e052018f00d929266a5b5b9fea8952c097985
Lil Devil <themax.06y@gmail.com>
2021-04-19 16:50:53 +0300

    Заголовок окна привязанный к ViewModel

---
+++ LuckyCurrency/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using LuckyCurrency.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuckyCurrency.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+        private string _title = "LuckyCurrency";
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        #endregion
+
+    }
+}
