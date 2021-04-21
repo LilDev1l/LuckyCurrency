8c1839dc9bd3715eff0ab25a1519c7e845520a94
Lil Devil <themax.06y@gmail.com>
2021-04-21 18:50:30 +0300

    Построенный свечной график на основе данных полученных посредством API | LuckyCurrencyTest

--- LuckyCurrencyTest/MainWindow.xaml.cs
+++ LuckyCurrencyTest/MainWindow.xaml.cs
@@ -2,6 +2,13 @@
 using System.Collections.ObjectModel;
 using FancyCandles;
 using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+using IO.Swagger.Model;
+using IO.Swagger.Api;
 
 namespace LuckyCurrencyTest
 {
@@ -12,16 +19,45 @@ namespace LuckyCurrencyTest
             InitializeComponent();
 
             ObservableCollection<ICandle> candles = new ObservableCollection<ICandle>();
-            DateTime t0 = new DateTime(2019, 6, 11, 23, 40, 0);
-            for (int i = 0; i < 100; i++)
+
+            var apiInstanceTime = new CommonApi();
+            var apiInstance = new KlineApi();
+            var timeServerSeconds = (long)((JObject) apiInstanceTime.CommonGetTime())["time_now"].ToObject<double>();
+            var timeServerTick = timeServerSeconds * 10000000;
+            var timeNow = DateTime.Now.Ticks;
+            var timeDuration = timeNow - timeServerTick;
+
+            var symbol = "BTCUSD";  // string | Contract type.
+            var interval = "1";  // string | Kline interval.
+            var from = timeServerSeconds - 200 * 60;  // decimal? | from timestamp.
+            var limit = 200;  // decimal? | Contract type. (optional) 
+
+            try
             {
-                double p0 = Math.Round(Math.Sin(0.3 * i) + 0.1 * i, 3);
-                double p1 = Math.Round(Math.Sin(0.3 * i + 1) + 0.1 * i, 3);
-                candles.Add(new Candle(t0.AddMinutes(i * 5),
-                            100 + p0, 101 + p0, 99 + p0, 100 + p1, i));
+                // Get kline
+                Object result = apiInstance.KlineGet(symbol, interval, (int)from, limit);
+                if (result is JObject jo)
+                {
+                    //Console.WriteLine(jo);
+                    List<KlineRes> klines = jo.ToObject<KlineBase>().Result;
+                    int i = 0;
+                    foreach (var kline in klines)
+                    {
+                        Console.WriteLine(kline);
+                        Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
+                        ; 
+                        
+                        candles.Add(new Candle(new DateTime((long)kline.OpenTime * 10000000 + timeDuration), double.Parse(kline.Open), double.Parse(kline.High), double.Parse(kline.Low), double.Parse(kline.Close), long.Parse(kline.Volume)));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception when calling KlineApi.KlineGet: " + e.Message);
             }
 
             DataContext = candles;
         }
     }
 }
+
