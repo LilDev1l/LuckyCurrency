9e75ce9ee5002397b7d79728d034ca2af581859b
Lil Devil <themax.06y@gmail.com>
2021-05-05 20:27:13 +0300

    Почистил LuckyCurrencyTestConsole

--- LuckyCurrencyTestConsole/Program.cs
+++ LuckyCurrencyTestConsole/Program.cs
@@ -11,28 +11,7 @@ namespace LuckyCurrencyTestConsole
     {
         public static void Main(string[] args)
         {
-            var exitEvent = new ManualResetEvent(false);
-            var url = new Uri("wss://stream.bytick.com/realtime_public");
-
-            using (var client = new WebsocketClient(url))
-            {
-
-                /*                client.ReconnectTimeout = TimeSpan.FromSeconds(50);
-                                client.ReconnectionHappened.Subscribe(info =>
-                                    Console.WriteLine($"Reconnection happened, type: {info.Type}"));
-                */
-
-                client.MessageReceived.Subscribe(msg =>
-                {
-                    Console.WriteLine($"Message received: {msg}");
-                });
-                client.Start();
-
-                client.Send("{\"op\":\"subscribe\",\"args\":[\"trade.BTCUSDT\"]}");
-                //exitEvent.WaitOne();
-                Thread.Sleep(10000);
-                client.Stop(System.Net.WebSockets.WebSocketCloseStatus.Empty, "stop");
-            }
+            
         }
     }
 }
