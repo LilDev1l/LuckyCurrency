fae07db943e636d116a89a92d35018247d8eee00
Lil Devil <themax.06y@gmail.com>
2021-05-29 02:11:39 +0300

    Автоматическое создание функции в БД

--- LuckyCurrency/Data/ApplicationContext.cs
+++ LuckyCurrency/Data/ApplicationContext.cs
@@ -5,6 +5,10 @@ namespace LuckyCurrency.Data
 {
     class ApplicationContext : DbContext
     {
+        static ApplicationContext()
+        {
+            Database.SetInitializer<ApplicationContext>(new MyContextInitializer());
+        }
         public ApplicationContext() : base("DefaultConection")
         { }
             
@@ -12,4 +16,43 @@ namespace LuckyCurrency.Data
         public DbSet<Account> Accounts { get; set; }
         public DbSet<API_Key> API_Keys { get; set; }
     }
+
+    class MyContextInitializer : DropCreateDatabaseAlways<ApplicationContext>
+    {
+        protected override void Seed(ApplicationContext db)
+        {
+            string createFunction = @"CREATE FUNCTION [dbo].[GetAPI_Key]
+                                (
+                                    @login NVARCHAR(MAX),
+                                    @password NVARCHAR(MAX)
+                                )
+                                RETURNS @returntable TABLE
+                                (
+                                    id        INT,
+                                    publicKey NVARCHAR(MAX),
+                                    secretKey NVARCHAR(MAX),
+                                    accountId INT
+                                )
+                                AS
+                                BEGIN
+
+                                    INSERT @returntable
+
+                                    SELECT*
+                                    FROM API_Key api
+
+                                    WHERE api.Id = (SELECT top(1) u.Id
+                                            FROM Users u
+
+                                            WHERE u.Login = @login AND
+
+                                                u.Password = @password)
+	                                RETURN
+                                END";
+            db.Database.ExecuteSqlCommand(createFunction);
+            //db.SaveChanges();
+        }
+    }
 }
+
+
